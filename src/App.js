e80fe58de9f4418dbddbf5e6fcd29cb6e19bbfde
marcus <marcus.rodrigues10@hotmail.com>
2021-07-29 19:13:25 -0300

    Exercise 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Images from './Components/Images';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-
+        <Images />
       </div>
     );
   }
